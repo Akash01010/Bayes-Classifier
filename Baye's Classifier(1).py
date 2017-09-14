f66b4ccf49a7c8c605461a889e6f7e4150465a6a
Akash <akashagrawal01010@gmail.com>
2017-09-14 21:49:30 +0530

    sigma inverse calculated for 1st type of covariance maeix

--- Baye's Classifier(1).py
+++ Baye's Classifier(1).py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 
 
+####################################################################################### 1st Class ############################################################################################
 file1=open("LS_Group2/Class1.txt")
 data = []
 for line in file1:
@@ -10,9 +11,7 @@ for line in file1:
     numbers = [float(n) for n in number_strings]
     data.append(numbers)
 
-#class1.mean(axis=0)
 class1=np.array(data)
-#print (class1)
 
 mean1=class1.mean(axis=0)
 
@@ -20,6 +19,7 @@ variance1x=math.sqrt(sum((class1[i][0]-mean1[0])*(class1[i][0]-mean1[0]) for i i
 variance1y=math.sqrt(sum((class1[i][1]-mean1[1])*(class1[i][1]-mean1[1]) for i in range(len(class1))))
 
 
+####################################################################################### 2nd Class ############################################################################################
 file2=open("LS_Group2/Class2.txt")
 data = []
 for line in file2:
@@ -27,9 +27,7 @@ for line in file2:
     numbers = [float(n) for n in number_strings]
     data.append(numbers)
 
-#class1.mean(axis=0)
 class2=np.array(data)
-#print (class2)
 
 mean2=class2.mean(axis=0)
 
@@ -37,6 +35,7 @@ variance2x=math.sqrt(sum((class2[i][0]-mean2[0])*(class2[i][0]-mean2[0]) for i i
 variance2y=math.sqrt(sum((class2[i][1]-mean1[1])*(class2[i][1]-mean2[1]) for i in range(len(class2))))
 
 
+####################################################################################### 3rd Class ############################################################################################
 file3=open("LS_Group2/Class3.txt")
 data = []
 for line in file3:
@@ -44,9 +43,7 @@ for line in file3:
     numbers = [float(n) for n in number_strings]
     data.append(numbers)
 
-#class1.mean(axis=0)
 class3=np.array(data)
-#print (class3)
 
 mean3=class3.mean(axis=0)
 
@@ -54,10 +51,12 @@ variance3x=math.sqrt(sum((class3[i][0]-mean3[0])*(class3[i][0]-mean3[0]) for i i
 variance3y=math.sqrt(sum((class3[i][1]-mean3[1])*(class3[i][1]-mean3[1]) for i in range(len(class3))))
 
 
-var1=(variance1x+variance1y)/2
+var1=(variance1x+variance1y)/2    #see if ths shld be norm or avg
 var2=(variance2x+variance2y)/2
 var3=(variance3x+variance3y)/2
 
+
+###############################################################################################################################################################################################
 #varx=(variance1x+variance2x+variance3x)/3
 #vary=(variance1y+variance2y+variance3y)/3
 
@@ -65,12 +64,20 @@ sigma=np.matrix([[(var1*var1 + var2*var2 +var3*var3)/3,0],[0,(var1*var1 + var2*v
 sigma_inverse=sigma.I
 print(sigma_inverse)
 
+
+omega_=np.matmul(sigma_inverse,mean1-mean2)
+print(omega_)
 #print(sum(sum(class1[i] for i in range(1,len(class1))))/float(len(class1)))
+plt.subplot(331)
 plt.plot([class1[i][0] for i in range(len(class1))],[class1[i][1] for i in range(len(class1))],'o')
 plt.plot([class2[i][0] for i in range(len(class2))],[class2[i][1] for i in range(len(class2))],'o')
 plt.plot([class3[i][0] for i in range(len(class3))],[class3[i][1] for i in range(len(class3))],'o')
 
 
+plt.subplot(334)
+plt.plot([class1[i][0] for i in range(len(class1))],[class1[i][1] for i in range(len(class1))],'o')
+plt.plot([class2[i][0] for i in range(len(class2))],[class2[i][1] for i in range(len(class2))],'o')
+plt.plot([class3[i][0] for i in range(len(class3))],[class3[i][1] for i in range(len(class3))],'o')
 
 
-#plt.show()
+plt.show()
