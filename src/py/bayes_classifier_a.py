7dfd004393730e67622f27225c519eec70896bfa
am7 <aayushmishra777@gmail.com>
2017-09-16 01:13:21 +0530

    Almost completed part A

--- src/py/bayes_classifier_a.py
+++ src/py/bayes_classifier_a.py
@@ -1,87 +1,131 @@
+#	About: 
+#		This program classifies the data for different classes by
+#		assuming the case when Covariance Matrix = (sigma)^2*I
+
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
+#	Variable:
+#		classes - Array of training data for different classes
+#		mean - Array of mean vectors of different classes
+#		variance - Array of covariance matrix of different classes
 
-####################################################################################### 1st Class ############################################################################################
-file1=open("../../data/LS_Group2/Class1.txt")
-data = []
-for line in file1:
-    number_strings = line.split() #splits across whitespaces
-    numbers = [float(n) for n in number_strings]
-    data.append(numbers)
+classes=[]
+testData=[]
+mean=[]
+variance=[]
+lenClasses=[]
+dimension=2
+confusionMat=[]
+total_variance=0
 
-class1=np.array(data)
+#Funtion: calcPrereq
+#	
 
-data_train=[data[i] for i in range(len(class1))]
+def calcPrereq(filename):
+	global classes,mean,variance
 
-class1_train=np.array(data_train)
-mean1=class1_train.mean(axis=0)
+	file=open(filename)
+	data=[]
+	for line in file:
+		number_strings=line.split()
+		numbers=[float(n) for n in number_strings]
+		data.append(numbers)
 
-variance1x=math.sqrt(sum((class1_train[i][0]-mean1[0])*(class1_train[i][0]-mean1[0]) for i in range(len(class1_train)))/len(class1))
-variance1y=math.sqrt(sum((class1_train[i][1]-mean1[1])*(class1_train[i][1]-mean1[1]) for i in range(len(class1_train)))/len(class1))
+	tempClass=np.array(data)
+	data_train=[data[i] for i in range(long(0.75*len(tempClass)))]
+	tempTestData=[data[i] for i in range(long(0.75*len(tempClass))+1,len(tempClass))]
+	tempClassTrain=np.array(data_train)
+	tempMean=tempClassTrain.mean(axis=0)
+	tempVariance=[]
+	for j in range(len(tempMean)):
+		tempVariance.append(sum((tempClassTrain[i][j]-tempMean[j])*(tempClassTrain[i][j]-tempMean[j]) for i in range(len(tempClassTrain)))/len(tempClassTrain))
+	classes.append(tempClassTrain)
+	mean.append(tempMean)
+	variance.append(tempVariance)
+	lenClasses.append(len(tempClassTrain))
+	testData.append(tempTestData)
+
+def g(x,first,second):
+	val=1/2/total_variance;
+	first_term=0;
+	for i in range(dimension):
+		first_term+=x[i]*(mean[second][i]-mean[first][i])
+	first_term*=2
+	second_term=0;
+	for i in range(dimension):
+		second_term+=(mean[first][i]*mean[first][i])-(mean[second][i]*mean[second][i])
+	val*=first_term+second_term
+	if val<0:
+		return first
+	else:
+		return second
+
+def calcConfusion(first,second):
+	temp=[[0 for i in range(2)] for i in range(2)]
+	for i in range(len(testData[first])):
+		x=testData[first][i]
+		ret=g(x,first,second)
+		if ret==first:
+			temp[0][0]+=1
+		else:
+			temp[0][1]+=1
+	for i in range(len(testData[second])):
+		x=testData[second][i]
+		ret=g(x,second,first)
+		if ret==second:
+			temp[1][1]+=1
+		else:
+			temp[1][0]+=1
+	confusionMat.append(temp)
+	
+	
+calcPrereq("../../data/rd_group2/Class1.txt")
+calcPrereq("../../data/rd_group2/Class2.txt")
+calcPrereq("../../data/rd_group2/Class3.txt")
 
 
-
-####################################################################################### 2nd Class ############################################################################################
-file2=open("../../data/LS_Group2/Class2.txt")
-data = []
-for line in file2:
-    number_strings = line.split()#splits across whitespaces
-    numbers = [float(n) for n in number_strings]
-    data.append(numbers)
-
-class2=np.array(data)
-
-data_train=[data[i] for i in range(len(class2))]
-
-class2_train=np.array(data_train)
-mean2=class2_train.mean(axis=0)
-
-variance2x=math.sqrt(sum((class2_train[i][0]-mean2[0])*(class2_train[i][0]-mean2[0]) for i in range(len(class2_train)))/len(class2))
-variance2y=math.sqrt(sum((class2_train[i][1]-mean2[1])*(class2_train[i][1]-mean2[1]) for i in range(len(class2_train)))/len(class2))
+for i in range(len(classes)):
+	for j in range(dimension):
+		total_variance+=variance[i][j]
+total_variance/=len(classes)*dimension
+sigma=np.matrix(total_variance*np.identity(dimension))
 
 
-####################################################################################### 3rd Class ############################################################################################
-file3=open("../../data/LS_Group2/Class3.txt")
-data = []
-for line in file3:
-    number_strings = line.split() #splits across whitespaces
-    numbers = [float(n) for n in number_strings]
-    data.append(numbers)
-class3=np.array(data)
+for i in range(len(classes)):
+	for j in range(i+1,len(classes)):
+		calcConfusion(i,j)
 
-data_train=[data[i] for i in range(len(class3))]
+print(mean)
 
-class3_train=np.array(data_train)
-mean3=class3_train.mean(axis=0)
-
-variance3x=math.sqrt(sum((class3_train[i][0]-mean3[0])*(class3_train[i][0]-mean3[0]) for i in range(len(class3_train)))/len(class3))
-variance3y=math.sqrt(sum((class3_train[i][1]-mean3[1])*(class3_train[i][1]-mean3[1]) for i in range(len(class3_train)))/len(class3))
-
-
-###############################################################################################################################################################################################
-
-
-sigma=np.matrix([[(variance1x*variance1x + variance2x*variance2x)/2,0],[0,(variance2x*variance2x + variance2y*variance2y)/2]])
-sigma_inverse=sigma.I
-print(sigma_inverse)
-
-
-omega_=np.matmul(sigma_inverse,mean1-mean2)
-print(omega_)
+print(confusionMat)
 
 plt.subplot(331)
-plt.plot([class1[i][0] for i in range(len(class1))],[class1[i][1] for i in range(len(class1))],'o')
-plt.plot([class2[i][0] for i in range(len(class2))],[class2[i][1] for i in range(len(class2))],'o')
-plt.plot([class3[i][0] for i in range(len(class3))],[class3[i][1] for i in range(len(class3))],'o')
+plt.plot([classes[0][i][0] for i in range(len(classes[0]))],[classes[0][i][1] for i in range(len(classes[0]))],'.')
 
+plt.subplot(332)
+plt.plot([classes[1][i][0] for i in range(len(classes[1]))],[classes[1][i][1] for i in range(len(classes[1]))],'.')
+
+plt.subplot(333)
+plt.plot([classes[2][i][0] for i in range(len(classes[2]))],[classes[2][i][1] for i in range(len(classes[2]))],'.')
 
 plt.subplot(334)
-plt.plot([class1[i][0] for i in range(len(class1))],[class1[i][1] for i in range(len(class1))],'o')
-plt.plot([class2[i][0] for i in range(len(class2))],[class2[i][1] for i in range(len(class2))],'o')
-plt.plot([class3[i][0] for i in range(len(class3))],[class3[i][1] for i in range(len(class3))],'o')
+plt.plot([classes[0][i][0] for i in range(len(classes[0]))],[classes[0][i][1] for i in range(len(classes[0]))],'.')
+plt.plot([classes[1][i][0] for i in range(len(classes[1]))],[classes[1][i][1] for i in range(len(classes[1]))],'.')
 
+plt.subplot(335)
+plt.plot([classes[1][i][0] for i in range(len(classes[1]))],[classes[1][i][1] for i in range(len(classes[1]))],'.')
+plt.plot([classes[2][i][0] for i in range(len(classes[2]))],[classes[2][i][1] for i in range(len(classes[2]))],'.')
+
+plt.subplot(336)
+plt.plot([classes[0][i][0] for i in range(len(classes[0]))],[classes[0][i][1] for i in range(len(classes[0]))],'.')
+plt.plot([classes[2][i][0] for i in range(len(classes[2]))],[classes[2][i][1] for i in range(len(classes[2]))],'.')
+
+plt.subplot(337)
+plt.plot([classes[0][i][0] for i in range(len(classes[0]))],[classes[0][i][1] for i in range(len(classes[0]))],'.')
+plt.plot([classes[1][i][0] for i in range(len(classes[1]))],[classes[1][i][1] for i in range(len(classes[1]))],'.')
+plt.plot([classes[2][i][0] for i in range(len(classes[2]))],[classes[2][i][1] for i in range(len(classes[2]))],'.')
 
 plt.show()
 
